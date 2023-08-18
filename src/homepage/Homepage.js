8513c5bfe6b0965c58f59153522453a4a8b55452
Da-Hubb <josemariaofurum@gmail.com>
2023-08-18 12:21:14 +0100

    initial commit

---
+++ src/homepage/Homepage.js
@@ -0,0 +1,5 @@
+function Homepage() {
+    return <h1>This is the homepage</h1>
+}
+
+export default Homepage;
---
+++ src/product/Product.js
@@ -0,0 +1,5 @@
+function Product() {
+    return <h1>This is the product page</h1>
+}
+
+export default Product;
